9046eb1b7698ab8e5c814fdad3c8faa80cfa7427
Gabriel Gomez <batega21@gmail.com>
2025-05-21 17:12:33 -0300

    Pagination endpoint added

--- server.js
+++ server.js
@@ -33,6 +33,35 @@ app.get('/superheroes', async (req, res) => {
   }
 });
 
+app.get('/superheroes/search', async (req, res) => {
+  const query = req.query.q;
+  console.log('Searching for Heroes with query:', query);
+  const heroes = heroesLib.HEROES_API.filter((hero) =>
+    hero.name.toLowerCase().includes(query.toLowerCase())
+  );
+  if (heroes.length > 0) {
+    res.json(heroes);
+  } else {
+    res.status(404).json({ error: 'No heroes found' });
+  }
+});
+
+app.get('/superheroes/pagination', async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const heroes = heroesLib.HEROES_API.slice(startIndex, endIndex);
+  const totalHeroes = heroesLib.HEROES_API.length;
+
+  if (heroes.length > 0) {
+    res.json({ data: heroes, totalHeroes });
+  } else {
+    res.status(404).json({ error: 'No heroes found' });
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`Proxy server listening on port ${port}`);
 });
